7abc690dc034c4918d1e17020d5f6b0a47de1286
Osama Almas Rahman <o.almas@hotmail.com>
2022-07-15 20:54:37 +0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={
+            "https://scontent.fdac23-1.fna.fbcdn.net/v/t39.30808-6/283808653_10226210992478458_7739689751627844407_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGDYgghG_EcaS-C6Fi7I2loMd_UwpAV5Lgx39TCkBXkuDkJXqUD0jDyFslvTpNgj6E&_nc_ohc=BAsmvlvrqzEAX_-B_m4&_nc_ht=scontent.fdac23-1.fna&oh=00_AT9x6wjlacvBph5yqmqxjY-NvmwSFboAyyz08dFO3DUMuA&oe=62D5DE09"
+          }
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,9 +17,9 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
