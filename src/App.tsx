95f297c020afcc472f7f91807ae849034992e1d7
Osama Almas Rahman <o.almas@hotmail.com>
2022-07-16 13:26:43 +0600

    100% lighthouse

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img
-          src={
-            "https://scontent.fdac23-1.fna.fbcdn.net/v/t39.30808-6/283808653_10226210992478458_7739689751627844407_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGDYgghG_EcaS-C6Fi7I2loMd_UwpAV5Lgx39TCkBXkuDkJXqUD0jDyFslvTpNgj6E&_nc_ohc=BAsmvlvrqzEAX_-B_m4&_nc_ht=scontent.fdac23-1.fna&oh=00_AT9x6wjlacvBph5yqmqxjY-NvmwSFboAyyz08dFO3DUMuA&oe=62D5DE09"
-          }
+          src={"./logo512.png"}
           className="App-logo"
           alt="logo"
+          width="48vmin"
+          height="48vmin"
         />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
