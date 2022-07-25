c1f621a8dc5f35c5aab6b54228112c7cd370cd6f
Osama Almas Rahman <o.almas@hotmail.com>
2022-07-25 12:38:14 +0000

    pagination update

--- src/components/SetComponent.tsx
+++ src/components/SetComponent.tsx
@@ -2,12 +2,13 @@ import { useEffect, useMemo, useState } from "react";
 import { Helper } from "../utils/helper";
 import "./ExpansionsComponent.css";
 import { Link, useParams, useNavigate } from "react-router-dom";
+import { PagingComponent } from './PagingComponent/PagingComponent';
 
 export const SetComponent = () => {
   let params = useParams();
   let navigate = useNavigate();
   const [setCards, setSetCards] = useState<any[]>([]);
-  const [pageIndex, setPageIndex] = useState<number>(0);
+
   useEffect(() => {
     console.log(params);
     if (!params.setId) {
@@ -48,26 +49,12 @@ export const SetComponent = () => {
       }
     }
   }, []);
-  const populateSubtype = (card: any) => {
-    let returnVal = "";
-    if (card.subtype || card.subtypes) {
-      returnVal = " - ";
-      if (card.subtype) {
-        returnVal += card.subtype;
-      } else {
-        returnVal += card.subtypes?.join(" - ");
-      }
-    }
-    return returnVal;
-  };
 
-  const cardsPagingOnClick = (paramPageIndex: number) =>{
-    console.log(paramPageIndex);
-  }
+
 
   return (
     <div className="container">
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4 ">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4">
 
         {/* {setCards.map((card) => {
           return (
@@ -85,7 +72,7 @@ export const SetComponent = () => {
                   />
                   <div className="card-footer">
                     <small className="text-muted">
-                      {card.supertype + populateSubtype(card)}
+                      {card.supertype + Helper.populateSubtype(card)}
                     </small>
                   </div>
                 </div>
@@ -93,20 +80,9 @@ export const SetComponent = () => {
             </div>
           );
         })} */}
-        <nav aria-label="Page navigation example">
-          <ul className="pagination justify-content-center">
-            <li className="page-item cursor-pointer disabled">
-              <span className="page-link" tabIndex={-1} onClick={()=>cardsPagingOnClick(pageIndex - 1)}>Previous</span>
-            </li>
-            <li className="page-item cursor-pointer"><span className="page-link" onClick={()=>cardsPagingOnClick(1)}>1</span></li>
-            <li className="page-item" style={{padding:'0.375rem 0.75rem'}}>of</li>
-            <li className="page-item cursor-pointer"><span className="page-link" onClick={()=>cardsPagingOnClick(3)}>3</span></li>
-            <li className="page-item cursor-pointer">
-              <span className="page-link"  onClick={()=>cardsPagingOnClick(pageIndex + 1)}>Next</span>
-            </li>
-          </ul>
-        </nav>
+
       </div>
+      <PagingComponent></PagingComponent>
     </div>
   );
 };
