bd747a488c387e46eee990e1c0a683676f7ee20a
Osama Almas Rahman <o.almas@hotmail.com>
2022-07-25 07:43:20 +0000

    paginig init

--- src/components/SetComponent.tsx
+++ src/components/SetComponent.tsx
@@ -7,7 +7,7 @@ export const SetComponent = () => {
   let params = useParams();
   let navigate = useNavigate();
   const [setCards, setSetCards] = useState<any[]>([]);
-
+  const [pageIndex, setPageIndex] = useState<number>(0);
   useEffect(() => {
     console.log(params);
     if (!params.setId) {
@@ -60,10 +60,16 @@ export const SetComponent = () => {
     }
     return returnVal;
   };
+
+  const cardsPagingOnClick = (paramPageIndex: number) =>{
+    console.log(paramPageIndex);
+  }
+
   return (
     <div className="container">
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4 ">
-        {setCards.map((card) => {
+
+        {/* {setCards.map((card) => {
           return (
             <div className="col" key={card.id}>
               <Link to="/series" className="un-styled-anchor">
@@ -75,7 +81,7 @@ export const SetComponent = () => {
                     src={card?.images?.small}
                     className="card-img-top"
                     alt="..."
-                    // style={{ maxHeight: "3rem" }}
+                  // style={{ maxHeight: "3rem" }}
                   />
                   <div className="card-footer">
                     <small className="text-muted">
@@ -86,7 +92,20 @@ export const SetComponent = () => {
               </Link>
             </div>
           );
-        })}
+        })} */}
+        <nav aria-label="Page navigation example">
+          <ul className="pagination justify-content-center">
+            <li className="page-item cursor-pointer disabled">
+              <span className="page-link" tabIndex={-1} onClick={()=>cardsPagingOnClick(pageIndex - 1)}>Previous</span>
+            </li>
+            <li className="page-item cursor-pointer"><span className="page-link" onClick={()=>cardsPagingOnClick(1)}>1</span></li>
+            <li className="page-item" style={{padding:'0.375rem 0.75rem'}}>of</li>
+            <li className="page-item cursor-pointer"><span className="page-link" onClick={()=>cardsPagingOnClick(3)}>3</span></li>
+            <li className="page-item cursor-pointer">
+              <span className="page-link"  onClick={()=>cardsPagingOnClick(pageIndex + 1)}>Next</span>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
