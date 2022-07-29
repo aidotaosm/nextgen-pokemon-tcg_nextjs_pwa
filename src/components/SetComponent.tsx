299f01a78c3335356ea1ef00fdadd7ca568e4c7e
Osama Almas Rahman <o.almas@hotmail.com>
2022-07-30 00:06:37 +0600

    cleaned up

--- src/components/SetComponent.tsx
+++ src/components/SetComponent.tsx
@@ -13,7 +13,7 @@ export const SetComponent = () => {
   const [pageIndex, setPageIndex] = useState<number>(0);
   const [refPageNumber, setRefPageNumber] = useState<number>(0);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [selectedCard, setSelectedCard] = useState<any>({});
   const getSetCards = (paramPageIndex: number) => {
     setIsLoading(true);
     let pokemonSDKVariable = Helper.initializePokemonSDK();
@@ -82,33 +82,37 @@ export const SetComponent = () => {
         syncPagingReferences={syncPagingReferences}
         pageNumber={refPageNumber}
       ></PagingComponent>
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5 my-3">
         {setCards.data?.map((card: any) => {
           return (
-            <div className="col" key={card.id}>
-              <Link to="/series" className="un-styled-anchor">
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title mb-0">{card.name}</h5>
-                  </div>
-                  <img
-                    src={card?.images?.small}
-                    className="card-img-top"
-                    alt="..."
-                    // style={{ maxHeight: "3rem" }}
-                  />
-                  <div className="card-footer">
-                    <small className="text-muted">
-                      {card.supertype + Helper.populateSubtype(card)}
-                    </small>
-                  </div>
+            <div className="col mb-2" key={card.id}>
+              <div
+                className="card cursor-pointer"
+                data-bs-toggle="modal"
+                data-bs-target="#full-screen-card-modal"
+                onClick={(c) => {
+                  setSelectedCard(card);
+                }}
+              >
+                <div className="card-body">
+                  <h5 className="card-title mb-0">{card.name}</h5>
                 </div>
-              </Link>
+                <img
+                  src={card?.images?.small}
+                  className="card-img-top"
+                  alt="..."
+                  // style={{ maxHeight: "3rem" }}
+                />
+                <div className="card-footer">
+                  <small className="text-muted">
+                    {card.supertype + Helper.populateSubtype(card)}
+                  </small>
+                </div>
+              </div>
             </div>
           );
         })}
       </div>
-      asasas
       <PagingComponent
         pageChanged={pageChanged}
         paramPageSize={DEFAULT_PAGE_SIZE}
@@ -117,6 +121,46 @@ export const SetComponent = () => {
         syncPagingReferences={syncPagingReferences}
         pageNumber={refPageNumber}
       ></PagingComponent>
+
+      <div
+        className="modal fade"
+        id="full-screen-card-modal"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-xl">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">{selectedCard?.name}</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body d-flex justify-content-center">
+              <img
+                style={{ height: "75vh" }}
+                src={selectedCard?.images?.large}
+                alt="..."
+              />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              {/* <button type="button" className="btn btn-primary">
+                Save changes
+              </button> */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
