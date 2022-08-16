ecc2056545342895b8abdc962f96a6822bea263b
Osama Almas Rahman <o.almas@hotmail.com>
2022-08-17 00:55:45 +0600

    fallback image into lowres in offline support fixed

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,4 @@
 export const DEV_POKEMONTCG_IO_API_KEY: string =
   "688dc1af-ba77-42c1-8482-f2356ecb3c8c";
 export const DEFAULT_PAGE_SIZE: number = 20;
+export const DEFAULT_CARD_BACK_RATIO_TO_TWO_DECIMAL: string = "1.40";
