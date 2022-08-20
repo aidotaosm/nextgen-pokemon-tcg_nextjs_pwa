75bcff866281e4061c2c2b1b65ca7c4f6e799a81
Osama Almas Rahman <o.almas@hotmail.com>
2022-08-20 10:54:02 +0600

    text updated and error fix on 404 set

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Link from "../src/components/UtilityComponents/Link";
 import PokemonTCGCardsLaidOut from "../public/images/Pokemon-TCG-Cards-Laid-Out.webp";
 import codeRedemption from "../public/images/code-redemption-169.jpg";
 import buildPokemonTcgDecks from "../public/images/build-pokemon-tcg-decks-169-en.jpg";
-import CadBackSideways from "../public/images/Cardback-sideways.webp";
 
 const Index = () => {
   return (
@@ -19,23 +18,19 @@ const Index = () => {
                   blurDataURL={"/images/Cardback-sideways.webp"}
                 />
               </div>
-              {/* <img
-                src=".\images\Pokemon-TCG-Cards-Laid-Out.webp"
-                className="card-img-top "
-                alt="..."
-              /> */}
               <div className="card-body">
                 <h5 className="card-title text-decoration-none">
                   Browse Cards
                 </h5>
                 <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  Browse all sets of Pokemon TCG, search and your desired cards
+                  and more!
                 </p>
               </div>
               <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
+                <small className="text-muted">
+                  Check it out! New features coming every week!
+                </small>
               </div>
             </div>
           </Link>
@@ -49,20 +44,15 @@ const Index = () => {
                 blurDataURL={"/images/Cardback-sideways.webp"}
               />
             </div>
-            {/* <img
-              src=".\images\code-redemption-169.jpg"
-              className="card-img-top"
-              alt="..."
-            /> */}
             <div className="card-body">
               <h5 className="card-title">Deck Manager</h5>
               <p className="card-text">
-                This card has supporting text below as a natural lead-in to
-                additional content.
+                Pokemon TCG Deck builder. Build your deck from all the
+                expansions, from Base set to the latest one!
               </p>
             </div>
             <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted">Upcoming.</small>
             </div>
           </div>
         </div>
@@ -75,21 +65,15 @@ const Index = () => {
                 blurDataURL={"/images/Cardback-sideways.webp"}
               />
             </div>
-            {/* <img
-              src=".\images\build-pokemon-tcg-decks-169-en.jpg"
-              className="card-img-top"
-              alt="..."
-            /> */}
             <div className="card-body">
               <h5 className="card-title">Play Pokemon TCG</h5>
               <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
+                Play Pokemon TCG with your friend with decks built from the deck
+                builder!
               </p>
             </div>
             <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <small className="text-muted">Coming later.</small>
             </div>
           </div>
         </div>
