17bbb03a52dc0a163050f556fb22353f0fc00d85
Osama Almas Rahman <o.almas@hotmail.com>
2023-02-02 02:14:54 +0600

    default image width fix

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const DEV_POKEMONTCG_IO_API_KEY: string =
   "688dc1af-ba77-42c1-8482-f2356ecb3c8c";
 export const DEFAULT_PAGE_SIZE: number = 20;
-export const DEFAULT_CARD_BACK_RATIO: number = 1.3872832369942196;
+export const DEFAULT_CARD_BACK_RATIO: number = 1.38671875;
 export const SuperTypes = {
   Pokemon: "Pokémon",
 };
