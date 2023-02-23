6d4b82482ec18988ec99029bd9d82580f7faddc2
Osama Almas Rahman <o.almas@hotmail.com>
2023-02-23 16:33:15 +0600

    vercel config done

--- src/components/ImageComponent/ImageComponent.tsx
+++ src/components/ImageComponent/ImageComponent.tsx
@@ -15,9 +15,8 @@ export const ImageComponent: FunctionComponent<any> = ({
   fallBackType,
   fallbackImage,
   //change here to turn on or off image optimization - true is off
-  lqImageUnOptimize = true,
+  lqImageUnOptimize = process.env.IS_VERCEL === "true",
 }) => {
-  console.log(process.env.IS_VERCEL, "process.env.IS_VERCEL");
   const [imageSource, setImageSource] = useState(src);
   const [highQualityImageSource, setHighQualityImageSource] =
     useState(highQualitySrc);
