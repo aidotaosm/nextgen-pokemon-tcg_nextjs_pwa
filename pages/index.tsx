e95207ba475cf0ef70d1c3dd445dd0ace9194894
Osama Almas Rahman <o.almas@hotmail.com>
2023-03-15 23:42:02 +0600

    homepage revamp

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,15 @@ import { Helper } from "../src/utils/helper";
 import { useRouter } from "next/router";
 import { AppContext } from "../src/contexts/AppContext";
 import { LocalSearchComponent } from "../src/components/LocalSearchComponent/LocalSearchComponent";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+
 export const getStaticProps: GetStaticProps = async (context) => {
   const dynamicallyImportedJson: any = (
     await import("../public/Jsons/AllCards.json")
@@ -51,104 +60,56 @@ const Index = ({ setCards }: any) => {
           defaultSearchTerm={searchValue}
         />
       </div>
-      <div className="row row-cols-1 row-cols-md-2">
-        <div className="col">
+      <div className="">
+        <div className="">
           <Link href="/series" className="un-styled-anchor ">
-            <div className="card h-100 cursor-pointer">
-              <div className="card-body d-flex flex-column">
-                <h4 className="card-title text-decoration-none mb-0 text-center">
-                  Browse Cards
-                </h4>
-                <hr className="mb-0" />
-                <div className="flex-grow-1 flex-column justify-content-center d-flex">
-                  <ImageComponent
-                    src={swsh125}
-                    alt={"Browse cards"}
-                    className="w-100 h-auto card-img-top"
-                    lqImageUnOptimize={true}
-                  />
-                </div>
-              </div>
-              {/* <div className="card-img-top">
-         
-              </div> */}
-              <div className="card-footer p-3">
-                <span className="fs-6">
-                  Browse all expansions of Pokemon TCG, search and filter
-                  through your desired cards and more!
-                </span>
+            <div className=" cursor-pointer">
+              <h4 className=" text-decoration-none mb-0 text-center">
+                Browse Cards
+              </h4>
+              <div className="flex-grow-1 flex-column justify-content-center d-flex">
+                <ImageComponent
+                  src={swsh125}
+                  alt={"Browse cards"}
+                  className=""
+                  lqImageUnOptimize={true}
+                />
               </div>
             </div>
           </Link>
         </div>
-        <div className="col">
-          <div className="card h-100">
-            <div className="card-body">
-              <h4 className="card-title mb-0 text-center">
-                Today's Featured Cards!
-              </h4>
-            </div>
-            <div className="card-img-top px-3 pb-3">
-              <CarouselComponent isLandingPage={true}>
+        <div className="">
+          <h4 className=" mb-0 text-center">Today's Featured Cards!</h4>
+          <div className="px-3 pb-3">
+            <CarouselProvider
+              naturalSlideWidth={100}
+              naturalSlideHeight={125}
+              totalSlides={5}
+            >
+              <Slider>
                 {setCards?.map((card: any, index: number) => (
-                  <Fragment key={card.id}>
-                    <div
-                      className={
-                        "carousel-item " + (index == 0 ? "active" : "")
-                      }
+                  <Slide index={index} key={card.id}>
+                    <Link
+                      href={"/card/" + card.id}
+                      style={{ margin: "auto", maxWidth: "25rem" }}
                     >
-                      <div
-                        className=""
-                        style={{ margin: "auto", maxWidth: "25rem" }}
-                        onClick={(e) => {
-                          e.stopPropagation();
-                        }}
-                      >
-                        <Link
-                          href={"/card/" + card.id}
-                          className="un-styled-anchor "
-                        >
-                          <ImageComponent
-                            src={card?.images?.small}
-                            highQualitySrc={card?.images?.large}
-                            alt={card.name}
-                            width={734}
-                            height={1024}
-                            blurDataURL={defaultBlurImage}
-                            className="h-auto w-100"
-                          />
-                        </Link>
-                      </div>
-                    </div>
-                  </Fragment>
+                      <ImageComponent
+                        src={card?.images?.small}
+                        highQualitySrc={card?.images?.large}
+                        alt={card.name}
+                        width={734}
+                        height={1024}
+                        blurDataURL={defaultBlurImage}
+                      />
+                    </Link>
+                  </Slide>
                 ))}
-              </CarouselComponent>
-            </div>
+              </Slider>
+              <ButtonBack>Back</ButtonBack>
+              <ButtonNext>Next</ButtonNext>
+            </CarouselProvider>
           </div>
         </div>
-        {/* <div className="col opacity-50">
-          <div className="card h-100">
-            <div className="card-img-top">
-              <ImageComponent
-                src={buildPokemonTcgDecks}
-                alt={"Browse cards"}
-                blurDataURL={defaultBlurImage}
-                className="w-100 h-auto"
-                lqImageUnOptimize={true}
-              />
-            </div>
-            <div className="card-body">
-              <h5 className="card-title">Play Pokemon TCG</h5>
-              <p className="card-text">
-                Play Pokemon TCG with your friend with decks built from the deck
-                builder!
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">Coming later.</small>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
