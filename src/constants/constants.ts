277decbf20084d5696192382d31f02e81bf3aab4
Osama Almas Rahman <o.almas@hotmail.com>
2023-03-26 06:28:44 +0600

    netlify changes made and search page optimized

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,7 +7,8 @@ export const DEFAULT_CARD_BACK_RATIO: number = 1.38671875;
 export const SuperTypes = {
   Pokemon: "Pokémon",
 };
-export const Netlify_DEFAULT_URL = "https://pokemon-tcg-osm.netlify.app/";
+//export const Netlify_DEFAULT_URL = "https://pokemon-tcg-osm.netlify.app/";
+export const Netlify_DEFAULT_URL = "https://www.nextgen-pokemon-tcg.pro/";
 export const Vercel_DEFAULT_URL = "https://www.nextgen-pokemon-tcg.pro/";
 export const APP_PRIMARY_URL = "www.nextgen-pokemon-tcg";
 export const VERCEL_PRIMARY_HOST = "pro";
