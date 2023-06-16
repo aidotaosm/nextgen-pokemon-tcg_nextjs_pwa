b80c83567e66038e8bc65331a30a038c41cf280f
Osama Almas Rahman <o.almas@hotmail.com>
2023-06-16 19:07:27 +0600

    fixed order of series

--- pages/series.tsx
+++ pages/series.tsx
@@ -9,6 +9,11 @@ export const getStaticProps: GetStaticProps = async () => {
   let { arrayOfSeries, sets } = await getExpansions();
   let totalNumberOfSets = 0;
   if (arrayOfSeries && arrayOfSeries[0]) {
+    arrayOfSeries.sort(function (a, b) {
+      let convertedA = new Date(a.releaseDate);
+      let convertedB = new Date(b.releaseDate);
+      return convertedB.getTime() - convertedA.getTime()
+    });
     arrayOfSeries[0].isOpen = true;
     totalNumberOfSets = arrayOfSeries
       .map((series) => (totalNumberOfSets = series.sets.length))
